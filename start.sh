9130d768207f2c193306861a01dd6af1b75c7b39
Bradly Sharpe <5461464+BradlySharpe@users.noreply.github.com>
2024-01-01 20:10:58 +1100

    Add Docker Compose file, start script, and sample .env file

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Stopping..."
+sudo docker-compose down
+
+echo "Removing all images, containers, and volumes..."
+#echo y | sudo docker system prune -a
+# sudo docker image rm microservice-remotive
+# sudo docker image rm microservice-dynamite
+sudo docker image rm microservice-ziprecruiter
+# sudo docker image rm microservice-sendmessage
+
+echo "Starting..."
+sudo docker-compose up #--force-recreate
